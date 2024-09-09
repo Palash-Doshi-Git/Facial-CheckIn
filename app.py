e78de7f95d996b03def6961eddee32716f77fa4d
Palash Doshi <113602320+Palash-Doshi-Git@users.noreply.github.com>
2024-09-09 23:23:05 +0530

    Update app.py
    Added Health Check

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ if os.environ.get('FLASK_ENV') == 'development':
     def favicon():
         return '', 204
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "healthy"}), 200
+
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
